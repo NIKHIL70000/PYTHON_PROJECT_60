956ac4ee5613f9287833656fb9f30fa10af5ba8e
NIKHIL70000 <73905088+NIKHIL70000@users.noreply.github.com>
2023-04-19 09:23:12 +0530

    Add files via upload

---
+++ coordinatesclick.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+events = [i for i in dir(cv) if 'EVENT' in i]
+print( events )
+import cv2
+def Capture_Event(event, x, y, flags, params):
+	# If the left mouse button is pressed
+	if event == cv2.EVENT_LBUTTONDOWN:
+		# Print the coordinate of the
+		# clicked point
+		print(f"({x}, {y})")
+# The Main Function
+
+
+if __name__ == "__main__":
+    # Read the Image.
+    img = cv2.imread('How-to-draw-an-Anime-Character-e.png', 1)
+    # Show the Image
+    cv2.imshow('image', img)
+    # Set the Mouse Callback function, and call
+    # the Capture_Event function.
+    cv2.setMouseCallback('image', Capture_Event)
+    # Press any key to exit
+    cv2.waitKey(0)
+    # Destroy all the windows
+    cv2.destroyAllWindows()
